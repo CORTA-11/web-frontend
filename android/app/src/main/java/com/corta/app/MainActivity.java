c77183d03610d57c8df2794f8ac64e0ffdc93a9b
NipunSGeeTH <sangeethnipun385@gmail.com>
2026-09-23 00:37:31 +0530

    fix(mobile): link google-services.json in android/app and add Firebase startup safety guard

--- android/app/src/main/java/com/corta/app/MainActivity.java
+++ android/app/src/main/java/com/corta/app/MainActivity.java
@@ -1,5 +1,26 @@
 package com.corta.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        try {
+            if (FirebaseApp.getApps(this).isEmpty()) {
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setApplicationId("1:1234567890:android:corta")
+                        .setProjectId("corta-app")
+                        .setApiKey("AIzaSyDummyKeyForFallbackInit12345")
+                        .build();
+                FirebaseApp.initializeApp(this, options);
+            }
+        } catch (Exception e) {
+            // Log but don't crash
+            e.printStackTrace();
+        }
+        super.onCreate(savedInstanceState);
+    }
+}
